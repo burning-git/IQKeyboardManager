ea4eeae47e513fa6a02628efcfd4275954cdc60e
Burning <974201360@qq.com>
2019-01-06 13:45:54 +0800

    修改 customKeyboardDistanceFromTextFeild

--- Demo/Swift_Demo/AppDelegate.swift
+++ Demo/Swift_Demo/AppDelegate.swift
@@ -27,7 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // Override point for customization after application launch.
     
     IQKeyboardManager.shared.enable = true
-    IQKeyboardManager.shared.enableDebugging = true
+//    IQKeyboardManager.shared.enableDebugging = true
         
     return true
     }
